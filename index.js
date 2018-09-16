cfe356459b995ad06cb2ac1cc12ce2c43844f9b5
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-16 21:30:44 +0200

    Use package bindings to load .node

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 /** @type {Winservices} */
-const winservices = require("./build/Release/Winservices.node");
+const winservices = require("bindings")("winservices.node");
 
-console.time("enumServicesStatus");
 const services = winservices.enumServicesStatus();
-console.timeEnd("enumServicesStatus");
-
 for (const service of services) {
-    console.log(service);
-    // console.time("getServiceConfiguration");
-    // const config = winservices.getServiceConfiguration(service.name);
-    // console.timeEnd("getServiceConfiguration");
-    // console.log(config);
-    // console.log("\n");
-
-    console.time("getServiceTriggers");
+    console.log(`service name => ${service.name}`);
     const perf = winservices.getServiceTriggers(service.name);
-    console.timeEnd("getServiceTriggers");
 
     console.log(perf);
     console.log("\n\n");
